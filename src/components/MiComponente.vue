220305fd4a5b733043e4034e1f445c2dbdf23d0d
Damian <damiandanielvigo@gmail.com>
2020-02-14 21:42:19 -0300

    finalizado-vistas-directivas

--- src/components/MiComponente.vue
+++ src/components/MiComponente.vue
@@ -7,7 +7,30 @@
       <hr/>
 
     <h2>Reactividad</h2>
-    <input type="text" />
+    <input type="text" v-model="nombre" /> <!-- directiva v-model. Funcionalidad a elementos html -->
+    Mi nombre es: <strong>{{nombre}}</strong>
+
+    <h2>Directivas condicionales</h2>
+    <input type="number" v-model="edad" />
+    Mi edad es: <strong>{{edad}}</strong> <!-- dobles llaves de interpolación -->
+
+    <p class="alert-success-provisional" v-if="edad && edad >=18 && edad < 65">
+      Eres mayor de edad
+    </p>
+
+    <p class="alert-purple-provisional" v-else-if="edad && edad >=65">
+      Eres de avanzada edad
+    </p>
+
+     <!-- <p class="alert-danger-provisional" v-if="edad && edad < 18"> -->
+    <p class="alert-danger-provisional" v-else>
+      Eres menor de edad
+    </p>
+
+    <h2>Directiva iterativa</h2>
+      <ol>
+        <li v-for="pelicula in peliculas" :key="pelicula">{{pelicula}}</li> <!-- para evitar el warning usar la key, para que vue sepa a que elemento de html hace referencia -->
+     </ol> 
 
     </section>
 </template>
@@ -15,10 +38,13 @@
 <script>
 export default {
   name: 'MiComponente',
-  data() {
+  data() { // Modelo de datos
     return {
       titulo: 'Mi Componente', // titulo: // es una propiedad
-      subtitulo: 'Soy el subtitulo de mi componente' // subtitulo: // es una propiedad
+      subtitulo: 'Soy el subtitulo de mi componente', // subtitulo: // es una propiedad
+      nombre: '', // vincular el campo con el input
+      edad: 18,
+      peliculas: ['Gladiador', 'El señor de los anillos', '1917', 'Anabelle']
     }
   }
 }
---
+++ src/components/Peliculas.vue
@@ -0,0 +1,47 @@
+<template>
+  <section id="content">
+    <h2 class="subheader">Peliculas</h2>
+
+    <!-- Listado articulos -->
+    <div id="articles">
+      <article class="article-item" v-for="pelicula in peliculas" v-bind:key="pelicula.title">
+        <div class="image-wrap">
+          <img 
+            :src="pelicula.image" 
+            :alt="pelicula.title"
+            v-bind:title="pelicula.title"
+            />  <!-- bindear una propiedad de html :src ES LO MISMO QUE SOLO DOS PUNTOS-->
+        </div>
+
+        <h2>{{pelicula.title}}</h2> 
+        <span class="date" v-bind:class="{ // IMPORTANTE V-BIND Y : ES LO MISMO, SE PUEDE APLICAR CONDICIONES
+          yeargreen: pelicula.year >= 2020,
+          yearred: pelicula.year < 2020,
+        }" > 
+          {{pelicula.year}}
+        </span>
+        <a href="#">Leer más</a>
+        <div class="clearfix"></div>
+      </article>
+    
+    
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  
+  name: 'Peliculas',
+
+  data() {
+    return {
+      peliculas: [
+        {title: 'Gladiador', year: 2000, image:'https://i2.wp.com/frasesdelapelicula.com/wp-content/uploads/2013/09/gladiador.jpg?fit=1200%2C630&ssl=1'},
+        {title: 1917, year: 2020, image: 'https://es.web.img3.acsta.net/pictures/20/01/09/15/10/0234685.jpg'},
+        {title: 'El conjuro 3', year: 2020, image: 'https://depor.com/resizer/mU8zRNtZpwLbvYs9RtexiC5aRpM=/980x528/smart/arc-anglerfish-arc2-prod-elcomercio.s3.amazonaws.com/public/ATRBQGCVTRHQJAYCHJASMKTZK4.jpg' }
+      ]
+    }
+  }
+};
+</script>
