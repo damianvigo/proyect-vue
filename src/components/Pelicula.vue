7d88d3aa973d072add255c6419d5092835760fac
Damian <damiandanielvigo@gmail.com>
2020-02-15 17:11:03 -0300

    comunicacion-entre-componentes

---
+++ src/components/Pelicula.vue
@@ -0,0 +1,38 @@
+<template>
+  <article class="article-item">
+        <div class="image-wrap">
+          <img 
+            :src="pelicula.image" 
+            :alt="pelicula.title"
+            v-bind:title="pelicula.title"
+            />  <!-- bindear una propiedad de html :src ES LO MISMO QUE SOLO DOS PUNTOS-->
+        </div>
+
+        <h2>{{pelicula.title}}</h2> 
+        <span class="date" v-bind:class="{ // IMPORTANTE V-BIND Y : ES LO MISMO, SE PUEDE APLICAR CONDICIONES
+          yeargreen: pelicula.year >= 2020,
+          yearred: pelicula.year < 2020,
+        }" > 
+          {{pelicula.year}}
+        </span>
+        <a href="#">Leer más</a>
+
+        <input type="button" value="Marcar como favorita" 
+                @click="pasarFavoritaAlPadre(pelicula)"
+        />
+
+        <div class="clearfix"></div>
+      </article>
+</template>
+
+<script>
+export default {
+  name: 'Pelicula',
+  props: ['pelicula'],
+  methods: {
+    pasarFavoritaAlPadre(pelicula) {
+      this.$emit('favorita', pelicula);
+    }
+  }
+}
+</script>
