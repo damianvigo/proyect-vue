d1b9dfb5614099eb11a63e3af68c531f7c4e8b4a
Damian <damiandanielvigo@gmail.com>
2020-05-09 00:42:04 -0300

    update-let

--- src/components/CreateArticle.vue
+++ src/components/CreateArticle.vue
@@ -6,7 +6,7 @@ import axios from "axios";
 import Global from "../Global";
 import Article from "../models/Article";
 import { required } from "vuelidate/lib/validators";
-import swal from 'sweetalert';
+import swal from "sweetalert";
 
 export default {
   name: "CreateArticle",
@@ -44,26 +44,26 @@ export default {
       if (this.$v.$invalid) {
         return false;
       } else {
-        
         axios.post(this.url + "save", this.article).then(response => {
-          
           if (response.data.status == "success") {
-           
-           /*Se guarda la imagen si se ha guardado bien el articulo. Adjuntar el fichero recogido con formdata.  */
-            if(this.file != null && this.file != undefined && this.file != '') {
-              
+            /*Se guarda la imagen si se ha guardado bien el articulo. Adjuntar el fichero recogido con formdata.  */
+            if (
+              this.file != null &&
+              this.file != undefined &&
+              this.file != ""
+            ) {
               const formData = new FormData();
               formData.append("file0", this.file, this.file.name);
               var articleId = response.data.article._id;
-  
-               axios.post(this.url + "upload-image/" + articleId, formData)
+
+              axios
+                .post(this.url + "upload-image/" + articleId, formData)
                 .then(response => {
                   if (response.data.article) {
-
                     swal(
-                      'Articulo creado',
-                      'El articulo se ha creado correctamente :D',
-                      'success'
+                      "Articulo creado",
+                      "El articulo se ha creado correctamente :D",
+                      "success"
                     );
 
                     this.article = response.data.article;
@@ -71,28 +71,29 @@ export default {
                   } else {
                     // Mostrar alerta de error
                     swal(
-                      'Creación fallida',
-                      'El articulo no se ha guardado bien :(',
-                      'error'
+                      "Creación fallida",
+                      "El articulo no se ha guardado bien :(",
+                      "error"
                     );
                   }
                 })
                 .catch(error => {
                   console.log(error);
                 });
-           } else { /* Si no se envía un archivo, redirección */
+            } else {
+              /* Si no se envía un archivo, redirección */
 
               swal(
-                  'Articulo creado',
-                  'El articulo se ha creado correctamente :D',
-                  'success'
-                );
+                "Articulo creado",
+                "El articulo se ha creado correctamente :D",
+                "success"
+              );
 
               this.article = response.data.article;
               this.$router.push("/blog");
-            } 
+            }
           }
-        })
+        });
       }
     }
   }
--- src/components/EditArticle.vue
+++ src/components/EditArticle.vue
@@ -6,7 +6,7 @@ import axios from "axios";
 import Global from "../Global";
 import Article from "../models/Article";
 import { required } from "vuelidate/lib/validators";
-import swal from 'sweetalert';
+import swal from "sweetalert";
 
 export default {
   name: "EditArticle",
@@ -33,7 +33,7 @@ export default {
     }
   },
   mounted() {
-    var articleId = this.$route.params.id;
+    let articleId = this.$route.params.id;
     this.getArticle(articleId);
   },
   methods: {
@@ -41,69 +41,72 @@ export default {
       /* recibir los datos del formulario */
       this.file = this.$refs.file.files[0]; /* con refs sacando el file que esta vinculado al campo del formulario */
     },
-     getArticle(articleId) {
-      axios.get(this.url + 'article/' + articleId).then(res => {
-        if(res.data.status == 'success') {
+    getArticle(articleId) {
+      axios.get(this.url + "article/" + articleId).then(res => {
+        if (res.data.status == "success") {
           this.article = res.data.article;
         }
       });
     },
     save() {
       this.submitted = true;
-      var articleId = this.$route.params.id;
+      let articleId = this.$route.params.id;
       this.$v.$touch();
       if (this.$v.$invalid) {
         return false;
       } else {
-        
-        axios.put(this.url + "article/" + articleId, this.article).then(response => {
-          
-          if (response.data.status == "success") {
-           
-           /*Se guarda la imagen si se ha guardado bien el articulo. Adjuntar el fichero recogido con formdata.  */
-            if(this.file != null && this.file != undefined && this.file != '') {
-              
-              const formData = new FormData();
-              formData.append("file0", this.file, this.file.name);
-              var articleId = response.data.article._id;
-  
-               axios.post(this.url + "upload-image/" + articleId, formData)
-                .then(response => {
-                  if (response.data.article) {
+        axios
+          .put(this.url + "article/" + articleId, this.article)
+          .then(response => {
+            if (response.data.status == "success") {
+              /*Se guarda la imagen si se ha guardado bien el articulo. Adjuntar el fichero recogido con formdata.  */
+              if (
+                this.file != null &&
+                this.file != undefined &&
+                this.file != ""
+              ) {
+                const formData = new FormData();
+                formData.append("file0", this.file, this.file.name);
+                var articleId = response.data.article._id;
 
-                    swal(
-                      'Articulo editado',
-                      'El articulo se ha editado correctamente :D',
-                      'success'
-                    );
+                axios
+                  .post(this.url + "upload-image/" + articleId, formData)
+                  .then(response => {
+                    if (response.data.article) {
+                      swal(
+                        "Articulo editado",
+                        "El articulo se ha editado correctamente :D",
+                        "success"
+                      );
 
-                    this.article = response.data.article;
-                    this.$router.push("/articulo/" + this.article._id);
-                  } else {
-                    // Mostrar alerta de error
-                    swal(
-                      'Creación fallida',
-                      'El articulo no se ha editado bien :(',
-                      'error'
-                    );
-                  }
-                })
-                .catch(error => {
-                  console.log(error);
-                });
-           } else { /* Si no se envía un archivo, redirección */
+                      this.article = response.data.article;
+                      this.$router.push("/articulo/" + this.article._id);
+                    } else {
+                      // Mostrar alerta de error
+                      swal(
+                        "Creación fallida",
+                        "El articulo no se ha editado bien :(",
+                        "error"
+                      );
+                    }
+                  })
+                  .catch(error => {
+                    console.log(error);
+                  });
+              } else {
+                /* Si no se envía un archivo, redirección */
 
-              swal(
-                  'Articulo editado',
-                  'El articulo se ha editado correctamente :D',
-                  'success'
+                swal(
+                  "Articulo editado",
+                  "El articulo se ha editado correctamente :D",
+                  "success"
                 );
 
-              this.article = response.data.article;
-              this.$router.push("/articulo/" + this.article._id);
-            } 
-          }
-        })
+                this.article = response.data.article;
+                this.$router.push("/articulo/" + this.article._id);
+              }
+            }
+          });
       }
     }
   }
